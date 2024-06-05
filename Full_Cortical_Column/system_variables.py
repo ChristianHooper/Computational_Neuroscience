52f4f7a46f3ec06be8fe1492414ecc076a2b5c4f
ChristianHooper <ChristianHooper@protonmail.com>
2024-06-04 17:28:29 -0700

    Sync for offline work.

--- Full_Cortical_Column/system_variables.py
+++ Full_Cortical_Column/system_variables.py
@@ -5,11 +5,11 @@ wave_frequencies = {'delta':(.5,4),
                     'beta': (12, 30),
                     'gamma': (30, 100)}
 
-# Y-axis depth variables, modeled after the occipital cortex. (micro-meters)
-layers = {'i':(0, 150),
+# Y-axis layer depth variables, modeled after the occipital cortex. (micro-meters)
+layers = {'width': (30, 50),
+        'i':(0, 150),
         'ii': (150, 300),
         'iii':(300, 600),
         'iv': (600, 1000),
         'v':  (1000, 1400),
-        'vi': (1400, 1600),
-        'width': (30, 50)}
+        'vi': (1400, 1600)}
